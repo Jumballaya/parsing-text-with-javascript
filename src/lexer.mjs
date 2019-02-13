161af3c0c2adb5bcd1b3120661fa704af5eb0a2d
Patrick Burris <nchupacabra@gmail.com>
2019-02-13 09:25:51 -0600

    lexer #3

--- src/lexer.mjs
+++ src/lexer.mjs
@@ -5,17 +5,73 @@
  */
 import * as tokens from './tokens.mjs';
 
+const isNumber = n => {
+  if (n === ' ') return false;
+  return Number(n) >= 0 || Number(n) <= 9;
+};
+
 export default class Lexer {
   constructor(input) {
     this.input = input;
-    this.pos = -1;
+    this.pos = 0;
     this.readPos = 0;
     this.ch = '';
 
     this.readChar();
   }
 
-  nextToken() {}
+  nextToken() {
+    this.skipWhiteSpace();
+    let tok = {};
+
+    switch (this.ch) {
+      case '+':
+        tok = tokens.newToken(tokens.PLUS);
+        break;
+      case '-':
+        tok = tokens.newToken(tokens.MINUS);
+        break;
+      case '/':
+        tok = tokens.newToken(tokens.SLASH);
+        break;
+      case '*':
+        tok = tokens.newToken(tokens.ASTERISK);
+        break;
+      case '!':
+        tok = tokens.newToken(tokens.BANG);
+        break;
+      case '^':
+        tok = tokens.newToken(tokens.POWER);
+        break;
+      case '%':
+        tok = tokens.newToken(tokens.MODULO);
+        break;
+      case '(':
+        tok = tokens.newToken(tokens.LPAREN);
+        break;
+      case ')':
+        tok = tokens.newToken(tokens.RPAREN);
+        break;
+      case ';':
+        tok = tokens.newToken(tokens.EOF);
+        break;
+      default:
+        // Check if number
+        if (!Number.isNaN(this.ch)) {
+          let literal = '';
+          while (isNumber(this.ch)) {
+            literal += this.ch;
+            this.readChar();
+          }
+          tok = { ...tokens.newToken(tokens.NUMBER), literal };
+        } else {
+          tok = tokens.newToken(tokens.ILLEGAL);
+        }
+    }
+
+    this.readChar();
+    return tok;
+  }
 
   skipWhiteSpace() {
     while (this.ch === ' ') {
