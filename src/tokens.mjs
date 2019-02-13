307d1d32f60a3ccdfcc143a0f5d023db8e2bd493
Patrick Burris <nchupacabra@gmail.com>
2019-02-13 09:20:05 -0600

    tokens #1

---
+++ src/tokens.mjs
@@ -0,0 +1,16 @@
+/**
+ * Tokens
+ */
+
+export const PLUS = 'PLUS';
+export const MINUS = 'MINUS';
+export const BANG = 'BANG';
+export const ASTERISK = 'ASTERISK';
+export const SLASH = 'SLASH';
+export const MODULO = 'MODULO';
+export const POWER = 'POWER';
+export const LPAREN = 'LPAREN';
+export const RPAREN = 'RPAREN';
+export const NUMBER = 'NUMBER';
+export const ILLEGAL = 'ILLEGAL';
+export const EOF = 'EOF';
